f858bd10dc20acdc24ca5fcf75919818f9ba762f
Darc <darc.rosya@gmail.com>
2025-08-14 15:08:09 +0300

    root

--- README.md
+++
@@ -1,1 +0,0 @@
-# mazafaka_learning
--- src/utils/data_handle.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timezone
-
-def make_aware(dt: datetime | None) -> datetime | None:
-    if dt is None:
-        return None
-    if dt.tzinfo is None:
-        return dt.replace(tzinfo=timezone.utc)
-    return dt.astimezone(timezone.utc)
